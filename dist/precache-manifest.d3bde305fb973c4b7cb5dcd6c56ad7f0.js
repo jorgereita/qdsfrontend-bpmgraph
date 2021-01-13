d410ed38a9f1246281bf03390fd4caa804bf09c3
Jorge Reita <jorge.reita@avaldigitallabs.com>
2021-01-13 09:16:34 -0500

    testo for host

---
+++ dist/precache-manifest.d3bde305fb973c4b7cb5dcd6c56ad7f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3a8bb8715851d2669e",
+    "url": "/css/app.7b43eea7.css"
+  },
+  {
+    "revision": "68470a93062536c5d901",
+    "url": "/css/chunk-vendors.dbf5f499.css"
+  },
+  {
+    "revision": "612a2939763dabb6b6600f75543f7836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b3a8bb8715851d2669e",
+    "url": "/js/app.446a4b5c.js"
+  },
+  {
+    "revision": "68470a93062536c5d901",
+    "url": "/js/chunk-vendors.8e350699.js"
+  },
+  {
+    "revision": "83ff4f4040b849f6f7bb16c1ea91ec2d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
